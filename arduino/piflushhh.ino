9955f2e789e07eb225d4a405a4c8da33ab9894e8
Devid <45665085+thinkedinthesea@users.noreply.github.com>
2020-10-14 13:19:42 +0200

    Update piflushhh.ino

--- arduino/piflushhh.ino
+++ arduino/piflushhh.ino
@@ -1,21 +1,17 @@
 /*
- * PiFLUSHHH
- * thinkedinthesea
- * 
- * rev01
- * 010118
- * production release
- * 
- * rev02
- * 100920
- * major update on SR
+   PiFLUSHHH
+   thinkedinthesea
+
+
+   rev01
+   131020
+   production release
 */
 //attiny85 pins
-const int POWER_OUT = 0;
-const int POWER_LED = 1;
-const int WATCHDOG = 2;
-const int POWER_BTN = 3;
-const int SR = 4;
+const int MOSFET = 4;
+const int WD = 0;
+const int SWITCH = 2;
+const int ATTINY = 1;
 //define max watchdog wait in startup state
 const int WAITWD = 50;
 //define max watchdog wait in poweroff state
@@ -27,23 +23,22 @@ int tempo;
 //
 void setup() {
   //declare the pins
-  pinMode (POWER_OUT, OUTPUT);
-  pinMode (POWER_LED, OUTPUT);
-  pinMode (SR, OUTPUT);
-  pinMode (WATCHDOG, INPUT);
-  pinMode (POWER_BTN, INPUT);
+  pinMode (MOSFET, OUTPUT);
+  pinMode (ATTINY, OUTPUT);
+  pinMode (WD, INPUT);
+  pinMode (SWITCH, INPUT);
   //variables
   reboot = 0;
   restart = 0;
   tempo = 0;
   //pins
-  digitalWrite(POWER_LED, LOW);
-  digitalWrite(POWER_OUT, LOW);
+  digitalWrite(MOSFET, LOW);
+  digitalWrite(ATTINY, LOW);
 }
 //
 void loop() {
   //check for power button
-  while (digitalRead(POWER_BTN) == HIGH) {
+  while (digitalRead(SWITCH) == LOW) {
     // check for previous reboot state
     if (reboot == 1) {
       // reset the reboot variable
@@ -53,14 +48,13 @@ void loop() {
     //bla bla bla
   }
   //fire up the board
-  digitalWrite(POWER_OUT, HIGH);
+  digitalWrite(MOSFET, HIGH);
   //check for WATCHDOG signal HIGH
   checkWatchdog();
   if (restart == 0) {
     //if we are here, we correctly receive the watchdog signal HIGH
     //make POWER_LED HIGH without blink
     //check for watchdog signal and for power button
-    digitalWrite(POWER_LED, HIGH);
     checkWatchdogAndPower();
   }
   if (restart == 0) {
@@ -69,42 +63,35 @@ void loop() {
     //REBOOT: SR HIGH 50ms
     //POWEROFF: SR HIGH 200ms
     tempo = 0;
-    while (digitalRead(POWER_BTN) == LOW) {
+    while (digitalRead(SWITCH) == HIGH) {
       delay(10);
       tempo++;
     }
     if (tempo > 100) {
       //poweroff
       //SR high 200ms
-      digitalWrite(SR, HIGH);
+      digitalWrite(ATTINY, HIGH);
       delay(200);
-      digitalWrite(SR, LOW);
+      digitalWrite(ATTINY, LOW);
       //check for WD low
-      while (digitalRead(WATCHDOG) == HIGH) {
-        digitalWrite(POWER_LED, HIGH);
-        delay(100);
-        digitalWrite(POWER_LED, LOW);
-        delay(100);
+      while (digitalRead(WD) == HIGH) {
+
       }
       //kill the board
       delay(10000);
-      digitalWrite(POWER_OUT, LOW);
-      digitalWrite(POWER_LED, LOW);
+      digitalWrite(MOSFET, LOW);
       //reboot
       reboot = 1;
     } else if (tempo < 101) {
       //SR high 50ms
-      digitalWrite(SR, HIGH);
+      digitalWrite(ATTINY, HIGH);
       delay(50);
-      digitalWrite(SR, LOW);
+      digitalWrite(ATTINY, LOW);
       //check for WD low
-      while (digitalRead(WATCHDOG) == HIGH) {
-        digitalWrite(POWER_LED, HIGH);
-        delay(1000);
-        digitalWrite(POWER_LED, LOW);
-        delay(1000);
+      while (digitalRead(WD) == HIGH) {
+
       }
-      
+
     }
   }
 }
@@ -112,54 +99,42 @@ void loop() {
 ////////////////////////////////////////////////
 void checkWatchdog() {
   tempo = 1;
-  while (digitalRead(WATCHDOG) == LOW) {
-    digitalWrite(POWER_LED, HIGH);
-    delay(200);
-    digitalWrite(POWER_LED, LOW);
-    delay(200);
-    // increase tempo
+  while (digitalRead(WD) == LOW) {
     tempo++;
     restart = 0;
     if (tempo > WAITWD) {
       // no WATCHDOG reived, kill the power
-      digitalWrite(POWER_OUT, LOW);
-      digitalWrite(POWER_LED, LOW);
+      digitalWrite(MOSFET, LOW);
       restart = 1;
       break;
       // game over
       //while (1);
     }
   }
-  
+
 }
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 void checkWatchdogAndPower() {
-  while (digitalRead(POWER_BTN) == HIGH && restart == 0) {
+  while (digitalRead(SWITCH) == LOW && restart == 0) {
     tempo = 0;
-    if (digitalRead(WATCHDOG) == LOW) {
-      while (digitalRead(WATCHDOG) == LOW) {
-        digitalWrite(POWER_LED, HIGH);
-        delay(500);
-        digitalWrite(POWER_LED, LOW);
-        delay(500);
-        // increase tempo
+    if (digitalRead(WD) == LOW) {
+      while (digitalRead(WD) == LOW) {
         tempo++;
         if (tempo > WAITWD2) {
           // no WATCHDOG reived, kill the power
-          digitalWrite(POWER_OUT, LOW);
-          digitalWrite(POWER_LED, LOW);
+          digitalWrite(MOSFET, LOW);
           // game over
           // while (1);
           restart = 1;
           break;
         }
       }
-     //if (digitalRead(WATCHDOG) == HIGH){
+      //if (digitalRead(WATCHDOG) == HIGH){
       //digitalWrite(POWER_LED, HIGH);
-     }
-     if (restart == 0){
-      digitalWrite(POWER_LED, HIGH);
-     }
+    }
+    if (restart == 0) {
+      //digitalWrite(POWER_LED, HIGH);
     }
   }
+}
