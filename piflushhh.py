11b4e8cc47d383d5d5f73aa7a2ecff8ea487c55b
Devid <45665085+thinkedinthesea@users.noreply.github.com>
2020-10-22 14:57:04 +0200

    Update piflushhh.py

--- piflushhh.py
+++ piflushhh.py
@@ -1,65 +1,77 @@
 #!/usr/bin/python
-#
-#THINKEDINTHESEA
-#PiFLUSHHH
-#
-#rev05102020
-#
-#python3 temp.py -l <y/n> -s <seconds>
-#-l log save to log yes/no
-#-t scantime in seconds
-#-f temp for fan activation
-#
-import os
-import time
-from datetime import datetime
-import sys, argparse
+# -*- coding: utf-8 -*-
+
 import RPi.GPIO as GPIO
-#
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(13, GPIO.OUT)
-#
-def getOptions(args=sys.argv[1:]):
-    parser = argparse.ArgumentParser(description="Check for CPU Temp. Defaults no log, time 60 and temp 46.")
-    parser.add_argument("-l", "--log", help="y/n to save log.")
-    parser.add_argument("-t", "--time", help="Seconds to scan for temp.")
-    parser.add_argument("-f", "--fan", help="Max temp for FAN activation.")
-    options = parser.parse_args(args)
-    return options
-#
-options = getOptions(sys.argv[1:])
-#print(options.log)
-#print(options.fan)
-if not options.time:
-        options.time = "60"
-if not options.fan:
-        options.fan = "36"
-if options.time < 1
-        options.time = "1"
-#
-def measure_temp():
-        temp = os.popen("vcgencmd measure_temp").readline()
-        #temp2 = temp.replace("'C","")
-        return (temp.replace("temp=",""))
-#
-def write_log():
-        logg = open("/tmp/temp.log", "a")
-        now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        logg.write(dt_string + ";" + measure_temp())
-        logg.close()
-#
-def fan_activation():
-        ffan = measure_temp()
-        ffan = ffan.replace("\n","")
-        if (float(ffan.replace("'C","")) > float(options.fan)):
-          GPIO.output(13, True)
-        if (float(ffan.replace("'C","")) < float(options.fan)):
-          GPIO.output(13, False)
-#
-while True:
-      if (options.log == "y"):
-        write_log()
-      fan_activation()
-      time.sleep(int(options.time))
+import time
+import sys
+
+# Configuration
+FAN_PIN = 13  # BCM pin used to drive transistor's base
+WAIT_TIME = 1  # [s] Time to wait between each refresh
+FAN_MIN = 20  # [%] Fan minimum speed.
+PWM_FREQ = 25  # [Hz] Change this value if fan has strange behavior
+
+# Configurable temperature and fan speed steps
+tempSteps = [35, 60]  # [°C]
+speedSteps = [0, 100]  # [%]
+
+# Fan speed will change only of the difference of temperature is higher than hysteresis
+hyst = 1
+
+# Setup GPIO pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(FAN_PIN, GPIO.OUT, initial=GPIO.LOW)
+fan = GPIO.PWM(FAN_PIN, PWM_FREQ)
+fan.start(0)
+
+i = 0
+cpuTemp = 0
+fanSpeed = 0
+cpuTempOld = 0
+fanSpeedOld = 0
+
+# We must set a speed value for each temperature step
+if len(speedSteps) != len(tempSteps):
+    print("Numbers of temp steps and speed steps are different")
+    exit(0)
+
+try:
+    while 1:
+        # Read CPU temperature
+        cpuTempFile = open("/sys/class/thermal/thermal_zone0/temp", "r")
+        cpuTemp = float(cpuTempFile.read()) / 1000
+        cpuTempFile.close()
+
+        # Calculate desired fan speed
+        if abs(cpuTemp - cpuTempOld) > hyst:
+            # Below first value, fan will run at min speed.
+            if cpuTemp < tempSteps[0]:
+                fanSpeed = speedSteps[0]
+            # Above last value, fan will run at max speed
+            elif cpuTemp >= tempSteps[len(tempSteps) - 1]:
+                fanSpeed = speedSteps[len(tempSteps) - 1]
+            # If temperature is between 2 steps, fan speed is calculated by linear interpolation
+            else:
+                for i in range(0, len(tempSteps) - 1):
+                    if (cpuTemp >= tempSteps[i]) and (cpuTemp < tempSteps[i + 1]):
+                        fanSpeed = round((speedSteps[i + 1] - speedSteps[i])
+                                         / (tempSteps[i + 1] - tempSteps[i])
+                                         * (cpuTemp - tempSteps[i])
+                                         + speedSteps[i], 1)
+
+            if fanSpeed != fanSpeedOld:
+                if (fanSpeed != fanSpeedOld
+                        and (fanSpeed >= FAN_MIN or fanSpeed == 0)):
+                    fan.ChangeDutyCycle(fanSpeed)
+                    fanSpeedOld = fanSpeed
+            cpuTempOld = cpuTemp
+
+        # Wait until next refresh
+        time.sleep(WAIT_TIME)
+
+
+# If a keyboard interrupt occurs (ctrl + c), the GPIO is set to 0 and the program exits.
+except KeyboardInterrupt:
+    print("Fan ctrl interrupted by keyboard")
+    GPIO.cleanup()
+    sys.exit()
