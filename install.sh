2fc0d4f960d485a7d69635a22378c24771a05c34
Devid <45665085+thinkedinthesea@users.noreply.github.com>
2020-11-30 11:03:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 #
 #THINKEDINTHESEA
-#PiFUN
+#FLUSHHH
 #
 #rev03122019
 #rev10092020
 #rev16102020
+#rev30112020
 #
 if [ $(id -u) -ne 0 ]; then
 	echo "Installer must be run as root."
@@ -16,7 +17,7 @@ clear
 #
 echo "------------------------------------------------------------------"
 echo "This script downloads and install"
-echo "PiFLUSHHH python script"
+echo "FLUSHHH python script"
 echo
 echo "- Update package index files (apt-get update)."
 echo "- Install Python libraries."
@@ -33,16 +34,16 @@ if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then
 	exit 0
 fi
 #
-echo -n "Downloading, installing piFLUSHHH..."
+echo -n "Downloading, installing FLUSHHH..."
 # Download to tmpfile because might already be running
-curl -f -s -o /tmp/piflushhh.py https://raw.githubusercontent.com/thinkedinthesea/PiFLUSHHH/main/piflushhh.py
-curl -f -s -o /tmp/powermypi_uno.py https://raw.githubusercontent.com/thinkedinthesea/PiFLUSHHH/main/powermypi_uno.py
-curl -f -s -o /tmp/led.py https://raw.githubusercontent.com/thinkedinthesea/PiFLUSHHH/main/led.py
+curl -f -s -o /tmp/piflushhh.py https://raw.githubusercontent.com/thinkedinthesea/FLUSHHH/main/flushhh.py
+curl -f -s -o /tmp/powermypi_uno.py https://raw.githubusercontent.com/thinkedinthesea/FLUSHHH/main/switch.py
+curl -f -s -o /tmp/led.py https://raw.githubusercontent.com/thinkedinthesea/FLUSHHH/main/led.py
 if [ $? -eq 0 ]; then
-	mv /tmp/piflushhh.py /usr/local/bin
-	chmod 755 /usr/local/bin/piflushhh.py
-	mv /tmp/powermypi_uno.py /usr/local/bin
-	chmod 755 /usr/local/bin/powermypi_uno.py
+	mv /tmp/flushhh.py /usr/local/bin
+	chmod 755 /usr/local/bin/flushhh.py
+	mv /tmp/switch.py /usr/local/bin
+	chmod 755 /usr/local/bin/switch.py
 	mv /tmp/led.py /usr/local/bin
 	chmod 755 /usr/local/bin/led.py
 	echo "OK"
@@ -63,10 +64,10 @@ echo
 echo "Add cronjob..."
 echo
 ##
-crontab -l | grep -v "@reboot sudo /usr/bin/python3 /usr/local/bin/piflushhh.py &" | crontab -
-crontab -l | { cat; echo "@reboot sudo /usr/bin/python3 /usr/local/bin/piflushhh.py &"; } | crontab -
-crontab -l | grep -v "@reboot sudo /usr/bin/python3 /usr/local/bin/powermypi_uno.py &" | crontab -
-crontab -l | { cat; echo "@reboot sudo /usr/bin/python3 /usr/local/bin/powermypi_uno.py &"; } | crontab -
+crontab -l | grep -v "@reboot sudo /usr/bin/python3 /usr/local/bin/flushhh.py &" | crontab -
+crontab -l | { cat; echo "@reboot sudo /usr/bin/python3 /usr/local/bin/flushhh.py &"; } | crontab -
+crontab -l | grep -v "@reboot sudo /usr/bin/python3 /usr/local/bin/switch.py &" | crontab -
+crontab -l | { cat; echo "@reboot sudo /usr/bin/python3 /usr/local/bin/switch.py &"; } | crontab -
 crontab -l | grep -v "@reboot sudo /usr/bin/python3 /usr/local/bin/led.py &" | crontab -
 crontab -l | { cat; echo "@reboot sudo /usr/bin/python3 /usr/local/bin/led.py &"; } | crontab -
 ##
